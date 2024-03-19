81f6980709806d5d2500a6087e4ee5751e396bda
Ewerton Franco <ewerton.silva@usemobile.xyz>
2024-03-19 18:55:33 -0300

    feat: login is now a separate component

---
+++ components/auth/login.vue
@@ -0,0 +1,134 @@
+<script>
+import { useForm } from "vee-validate";
+import { storeToRefs } from "pinia";
+import { loginSchema } from "~/utils/schemas";
+
+export default {
+  setup() {
+    const router = useRouter();
+    const { authenticated } = storeToRefs(useAuthStore());
+
+    const { handleSubmit, resetForm } = useForm({
+      initialValues: { username: "", password: "" },
+      validationSchema: loginSchema,
+    });
+    const onSubmit = handleSubmit((formValues) => formValues);
+
+    return { router, authenticated, onSubmit, resetForm };
+  },
+  data() {
+    return {
+      isLoading: false,
+      formData: {},
+      remindUser: false,
+      wrongCredentialsMessage: "",
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        this.formData = await this.onSubmit();
+        if (!this.formData) return;
+
+        this.isLoading = true;
+        const {
+          data: { user },
+        } = await this.$axios.post("/api/login", this.formData);
+
+        if (user && !this.remindUser) {
+          sessionStorage.setItem("token", user?.token);
+        }
+
+        if (user && this.remindUser) {
+          const token = useCookie("token");
+          token.value = user?.token;
+        }
+
+        if (this.authenticated) {
+          await this.router.push("/");
+        }
+      } catch (error) {
+        this.wrongCredentialsMessage = error.response.data.message;
+
+        this.resetForm({
+          values: {
+            username: "",
+            password: "",
+          },
+        });
+      } finally {
+        this.isLoading = false;
+      }
+    },
+  },
+};
+</script>
+<template>
+  <UiModal>
+    <div class="login">
+      <img src="~/assets/img/LOGO.png" alt="Itera logo" class="logo" />
+
+      <div class="login__header">
+        <h3 class="heading__tertiary">Área do RH</h3>
+        <p class="body__primary">Insira suas credenciais de acesso:</p>
+      </div>
+
+      <form class="form">
+        <div class="form__control">
+          <label class="caption__primary">
+            Usuário
+            <BaseInputMask
+              name="username"
+              mask="999.999.999-99"
+              placeholder="Insira seu CPF"
+              :wrong-crendentials-message="wrongCredentialsMessage"
+            />
+          </label>
+        </div>
+
+        <div class="form__control">
+          <label class="caption__primary">
+            Senha
+            <BaseInputPassword
+              name="password"
+              placeholder="Insira sua senha"
+              :wrong-crendentials-message="wrongCredentialsMessage"
+            />
+          </label>
+        </div>
+
+        <div class="form__reminder">
+          <div>
+            <BaseCheckbox
+              input-id="remindUser"
+              @change="remindUser = !remindUser"
+            />
+            <label class="form__reminder--label" for="remindUser">
+              Lembre-me
+            </label>
+          </div>
+
+          <span>
+            <BaseButton label="Esqueci a senha" class="btn__primary--text" />
+          </span>
+        </div>
+
+        <BaseButton
+          label="Entrar"
+          class="btn__primary"
+          :loading="isLoading"
+          @click.prevent="login"
+        />
+      </form>
+
+      <div class="register">
+        <span>Não tem cadastro?</span>
+        <span>
+          <BaseButton label="Cadastre-se" class="btn__primary--text" />
+        </span>
+      </div>
+    </div>
+  </UiModal>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ pages/auth.vue
@@ -0,0 +1,85 @@
+<script lang="ts">
+export default {};
+</script>
+
+<template>
+  <div class="container">
+    <img
+      src="~/assets/img/login-feature.png"
+      class="container__detail--1"
+      alt="container details"
+    />
+    <img
+      src="~/assets/img/login-feature.png"
+      class="container__detail--2"
+      alt="container details"
+    />
+
+    <!--    <AuthLogin />-->
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  overflow: hidden;
+  position: relative;
+  background-color: $color-brand-primary-0;
+  width: 100vw;
+  height: 100vh;
+
+  display: grid;
+  place-content: center;
+
+  &__detail--1 {
+    transform: rotate(180deg);
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+
+  &__detail--2 {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+  }
+}
+
+.login {
+  width: 35rem;
+  display: grid;
+  gap: $spacing-md;
+}
+
+.logo {
+  max-width: 20rem;
+  justify-self: center;
+}
+
+.form {
+  display: grid;
+  gap: $spacing-md;
+
+  &__reminder {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    & > div {
+      display: flex;
+      gap: 0.8rem;
+    }
+
+    &--label {
+      color: $color-neutral-neutral-2;
+      display: flex;
+      gap: 0.8rem;
+    }
+  }
+}
+
+.register {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
