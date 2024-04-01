d22fea0d9591b241b38350d2679eb0f158ed7ba8
Ewerton Franco <ewerton.silva@usemobile.xyz>
2024-04-01 09:36:48 -0300

    refactor: updated user outcome

--- stores/auth.store.ts
+++ stores/auth.store.ts
@@ -1,21 +1,22 @@
+import api from "~/services/api.service";
+
 export type User = {
-  email: string;
-  firstName: string;
-  gender: string;
   id: number;
+  cnpj: string;
+  email: string;
   image: string;
-  lastName: string;
-  role: string;
   token: string;
-  username: string;
+  gender: string;
+  lastName: string;
+  firstName: string;
+  companyName: string;
+  role: string | string[];
 };
 
 type AuthState = {
   user: User | null;
   authenticated: boolean;
 };
-
-export type AuthValues = { username: string; password: string };
 
 export const useAuthStore = defineStore("auth", {
   state: (): AuthState => {
@@ -26,27 +27,33 @@ export const useAuthStore = defineStore("auth", {
   },
   persist: true,
   actions: {
-    async authenticateUser(authValues: AuthValues) {
-      const { data: userInfo }: any = await useFetch(
-        "https://dummyjson.com/auth/login",
-        {
-          method: "post",
-          headers: { "Content-Type": "application/json" },
-          body: { username: "kminchelle", password: "0lelplR" },
-        },
-      );
+    authenticateUser() {
+      const userInfo = {
+        id: 15,
+        gender: "female",
+        lastName: "Xavier",
+        firstName: "Felipe",
+        cnpj: "01.270.742/0001-08",
+        email: "felipe.xavier@usemobile.com.br",
+        companyName: "Usemobile Soluções em Tecnologia",
+        image: "https://randomuser.me/api/portraits/women/39.jpg",
+        token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUsInVzZXJuYW1lIjoia21pbmNoZWxsZSIsImVtYWlsIjoia21pbmNoZWxsZUBxcS5jb20iLCJmaXJzdE5hbWUiOiJKZWFubmUiLCJsYXN0TmFtZSI6IkhhbHZvcnNvbiIsImdlbmRlciI6ImZlbWFsZSIsImltYWdlIjoiaHR0cHM6Ly9yb2JvaGFzaC5vcmcvSmVhbm5lLnBuZz9zZXQ9c2V0NCIsImlhdCI6MTcxMTc3Mzg4NiwiZXhwIjoxNzExNzc3NDg2fQ.GNIAwEDvoFZ5AtotEr9fPvGP3NVrjPiJha5R03bbuGI",
+      };
 
-      if (userInfo.value) {
-        this.user = { ...userInfo.value, role: ["manager"] };
+      if (userInfo) {
+        this.user = { ...userInfo, role: ["manager"] };
         this.authenticated = true;
 
         return this.user;
       }
     },
     logUserOut() {
-      const token = useCookie("token");
+      const cookiesToken = useCookie("token");
+      sessionStorage.removeItem("token");
+
       this.authenticated = false;
-      token.value = null;
+      cookiesToken.value = null;
       this.user = null;
     },
   },
