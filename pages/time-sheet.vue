d5c5d456a97a31864106610365dc82258303d288
Ewerton Franco <ewerton.silva@usemobile.xyz>
2024-04-03 17:29:00 -0300

    feat: added tabs on time sheet page

--- pages/time-sheet.vue
+++ pages/time-sheet.vue
@@ -1,11 +1,34 @@
 <script lang="ts">
-export default {};
+import PendingAdjustments from "~/components/time-sheet/pending-adjustments.vue";
+import ClockOverview from "~/components/time-sheet/clock-overview.vue";
+
+export default {
+  data() {
+    return {
+      tabs: [
+        {
+          label: "Ajustes pendentes",
+          icon: "pi-pencil",
+          component: PendingAdjustments,
+        },
+        {
+          label: "Todos os pontos",
+          icon: "pi-users",
+          component: ClockOverview,
+        },
+        {
+          label: "Busca avançada",
+          icon: "pi-search",
+          component: PendingAdjustments,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
-  <UiModal class="login fadein animation-duration-500">
-    <h1 class="heading__primary">Pontos</h1>
-  </UiModal>
+  <BaseTabs class="fadein animation-duration-500" :tabs-data="tabs" />
 </template>
 
 <style scoped lang="scss"></style>
