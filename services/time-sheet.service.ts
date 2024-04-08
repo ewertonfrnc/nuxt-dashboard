cd236d0709c30c3d8e87ae7a187b9546bde28376
Ewerton Franco <ewerton.silva@usemobile.xyz>
2024-04-08 10:00:28 -0300

    feat(time-sheet): ✨ updated request params for getting pending adjustments

--- services/time-sheet.service.ts
+++ services/time-sheet.service.ts
@@ -1,9 +1,19 @@
 import api from "~/services/api.service";
+import { QueryParams } from "~/interfaces/time-sheet/time-sheet.interface";
 
 class TimeSheetService {
-  fetchPendingAdjustments() {
-    return api().get("/api/time-sheet/pending");
+  fetchPendingAdjustments(params: QueryParams) {
+    const { name, currentBalance, totalRequests } = params;
+
+    return api().get(`/api/time-sheet/pending`, {
+      params: {
+        page: 1,
+        limit: 10,
+        name: name?.value || "",
+        currentBalance: currentBalance?.value || "",
+        totalRequests: totalRequests?.value || "",
+      },
+    });
   }
 }
-
 export default new TimeSheetService();
