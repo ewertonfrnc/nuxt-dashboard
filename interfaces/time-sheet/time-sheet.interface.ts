63943b69dd97f17cd03e972c8fbddf54820801ba
Ewerton Franco <ewerton.silva@usemobile.xyz>
2024-04-08 10:00:42 -0300

    feat(time-sheet): ✨ created type for query params

---
+++ interfaces/time-sheet/time-sheet.interface.ts
@@ -0,0 +1,6 @@
+export type QueryParams = {
+  global: { value: string; matchMode: string };
+  name: { value: string; matchMode: string };
+  currentBalance: { value: string; matchMode: string };
+  totalRequests: { value: string; matchMode: string };
+};
