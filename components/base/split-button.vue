75a0c7299957b0a7b3228c4c2cb8f8e4d0b56aac
Ewerton Franco <ewerton.silva@usemobile.xyz>
2024-04-12 09:19:52 -0300

    feat(home): ✨ created split-button.vue

--- components/base/split-button.vue
+++ components/base/split-button.vue
@@ -2,21 +2,36 @@
   <div class="split">
     <BaseButton
       label="Reprovar"
-      :disabled="disableLeftBtn"
-      class="btn__primary--outlined split__button-left"
-      @click.stop="leftButtonHandler"
+      :class="[
+        'btn__primary--outlined',
+        'split__button-left',
+        selectedBtn === 'left' && 'active',
+        (approveAll || disableLeftBtn) && 'inactive',
+      ]"
+      @click.stop="leftButtonHandler(request)"
     />
     <BaseButton
       label="Aprovar"
-      :disabled="disableRightBtn"
-      class="btn__primary--outlined split__button-right"
-      @click.stop="rightButtonHandler"
+      :class="[
+        'btn__primary--outlined',
+        'split__button-right',
+        (approveAll || selectedBtn === 'right') && 'active',
+        disableRightBtn && 'inactive',
+      ]"
+      @click.stop="rightButtonHandler(request)"
     />
   </div>
 </template>
 
 <script lang="ts">
+import { PropType } from "vue";
+import { Request } from "~/interfaces/time-sheet/time-sheet.interface";
+
 export default {
+  props: {
+    request: { type: Object as PropType<Request>, required: true },
+    approveAll: { type: Boolean, default: false, required: true },
+  },
   emits: ["button-handler"],
   data() {
     return {
@@ -26,13 +41,19 @@ export default {
     };
   },
   methods: {
-    leftButtonHandler() {
+    leftButtonHandler(request: Request) {
       this.selectedBtn = "left";
-      this.$emit("button-handler", this.selectedBtn);
+      this.disableRightBtn = true;
+
+      request.approved = false;
+      this.$emit("button-handler", request);
     },
-    rightButtonHandler() {
+    rightButtonHandler(request: Request) {
       this.selectedBtn = "right";
-      this.$emit("button-handler", this.selectedBtn);
+      this.disableLeftBtn = true;
+
+      request.approved = true;
+      this.$emit("button-handler", request);
     },
   },
 };
@@ -43,14 +64,15 @@ export default {
   display: flex;
   align-items: center;
 
-  .active {
-    background-color: map-get($color-scheme-light, "$color-brand-primary-4");
-    color: map-get($color-scheme-light, "$color-neutral-neutral-7");
-  }
-
   &__button-left {
     border-bottom-right-radius: 0;
     border-top-right-radius: 0;
+    border-right-width: 1px;
+
+    &:hover,
+    &:focus {
+      border-right-width: 1px;
+    }
   }
 
   &__button-right {
@@ -58,9 +80,21 @@ export default {
     border-top-left-radius: 0;
     border-left-width: 0;
 
-    &:hover {
+    &:hover,
+    &:focus {
       border-left-width: 0;
     }
+  }
+
+  .inactive {
+    background-color: transparent;
+    color: map-get($color-scheme-light, "$color-neutral-neutral-3");
+    border-color: map-get($color-scheme-light, "$color-neutral-neutral-3");
+  }
+
+  .active {
+    background-color: map-get($color-scheme-light, "$color-brand-primary-0");
+    color: map-get($color-scheme-light, "$color-neutral-neutral-7");
   }
 }
 </style>
