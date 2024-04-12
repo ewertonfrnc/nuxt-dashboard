9dbfb809d57162d53dea26cbabd0a15619175e4e
Ewerton Franco <ewerton.silva@usemobile.xyz>
2024-04-12 09:15:12 -0300

    feat(home): ✨ updated http call for paginator

--- interfaces/time-sheet/time-sheet.interface.ts
+++ interfaces/time-sheet/time-sheet.interface.ts
@@ -1,4 +1,5 @@
 export type QueryParams = {
+  page: number;
   global: { value: string; matchMode: string };
   name: { value: string; matchMode: string };
   currentBalance: { value: string; matchMode: string };
@@ -14,28 +15,32 @@ export type PendingAdjust = {
   };
 };
 
-export type Check = {
+export type CheckInOut = {
   time: string;
   manipulated: boolean;
 };
 
+export type DayDetails = {
+  total: string;
+  negative: boolean;
+  checkins: CheckInOut[];
+  checkouts: CheckInOut[];
+};
+
 export type Request = {
+  id: number;
   date: string;
-  before: {
-    total: string;
-    checkins: Check[];
-    checkouts: Check[];
-  };
-  after: {
-    total: string;
-    checkins: Check[];
-    checkouts: Check[];
-  };
+  negative: boolean;
+  approved: boolean;
+  before: DayDetails;
+  after: DayDetails;
 };
 
 export type User = {
   userId: number;
   name: string;
   role: string;
+  currentBalance: string;
+  totalRequests: number;
   requests: Request[];
 };
--- services/time-sheet.service.ts
+++ services/time-sheet.service.ts
@@ -1,13 +1,16 @@
 import api from "~/services/api.service";
-import { QueryParams } from "~/interfaces/time-sheet/time-sheet.interface";
+import {
+  QueryParams,
+  Request,
+} from "~/interfaces/time-sheet/time-sheet.interface";
 
 class TimeSheetService {
   fetchPendingAdjustments(params: QueryParams) {
-    const { name, currentBalance, totalRequests } = params;
+    const { page, name, currentBalance, totalRequests } = params;
 
     return api().get(`/api/time-sheet/pending`, {
       params: {
-        page: 1,
+        page,
         limit: 10,
         name: name?.value || "",
         currentBalance: currentBalance?.value || "",
@@ -18,6 +21,10 @@ class TimeSheetService {
 
   fetchUserPendingAdjustments(userId: number) {
     return api().get(`/api/time-sheet/pending/${userId}`);
+  }
+
+  updatePendingAdjustments(userId: number, payload: Request[]) {
+    return api().put(`/api/time-sheet/pending/${userId}`, payload);
   }
 }
 export default new TimeSheetService();
