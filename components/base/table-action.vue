0348ac6d06d0f8a0eb0a9474a9a8636b95974088
Ewerton Franco <ewerton.silva@usemobile.xyz>
2024-04-18 11:47:51 -0300

    feat(time-sheet): ✨ created the table-action.vue component

---
+++ components/base/table-action.vue
@@ -0,0 +1,28 @@
+<template>
+  <span class="body__primary">
+    <i
+      v-tooltip.top="{
+        value: tooltipText,
+        pt: { root: 'tooltip' },
+      }"
+      :class="['pi', icon]"
+      @click="actionHandler(data)"
+    />
+  </span>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    tooltipText: { type: String, default: "", required: true },
+    icon: { type: String, default: "", required: true },
+    data: { type: Object, default: () => {}, required: true },
+  },
+  emits: ["action-handler"],
+  methods: {
+    actionHandler(data: Object) {
+      this.$emit("action-handler", data);
+    },
+  },
+};
+</script>
