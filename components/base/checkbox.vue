267da1284ed1ff1280f1182179fea0c072b0ee00
Ewerton Franco <ewerton.silva@usemobile.xyz>
2024-04-18 11:35:02 -0300

    feat(checkbox): ✨ updated checkbox.vue logic

--- components/base/checkbox.vue
+++ components/base/checkbox.vue
@@ -1,21 +1,31 @@
-<script lang="ts">
-export default {
-  data() {
-    return {
-      checked: false,
-    };
-  },
-};
-</script>
-
 <template>
   <Checkbox
     v-model="checked"
     :binary="true"
     :pt="{
-      root: 'root',
-      input: 'checkbox',
+      root: ({ context }) => {
+        return `checkbox ${context.checked && 'checkbox--checked'}`;
+      },
+      input: 'checkbox__input',
       icon: 'checkbox__icon',
     }"
+    @click="toggleChecked"
   />
 </template>
+
+<script lang="ts">
+export default {
+  emits: ["checkbox-value"],
+  data() {
+    return {
+      checked: false,
+    };
+  },
+  methods: {
+    toggleChecked() {
+      this.checked = !this.checked;
+      this.$emit("checkbox-value", this.checked);
+    },
+  },
+};
+</script>
