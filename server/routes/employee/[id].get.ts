1ecc89dfa5e6c99b445f728cea98f673763abd64
Ewerton Franco <ewerton.silva@usemobile.xyz>
2024-05-06 07:57:34 -0300

    feat(employees): ✨ updated server with user info

--- server/routes/employee/[id].get.ts
+++ server/routes/employee/[id].get.ts
@@ -5,6 +5,22 @@ export default defineEventHandler((event) => {
       name: "Ana Maria Martins Rocha",
       role: "Nutricionista",
       photo: "https://randomuser.me/api/portraits/women/92.jpg",
+      personalData: {
+        fullname: "Ana Maria Martins Rocha",
+        preferredName: "",
+        nickname: "Aninha",
+        cpf: "898.093.076-32",
+        rg: "44.174.917-3",
+        birthDate: "05/05/2005",
+        birthCity: "Conselheiro Lafaiete",
+        ethnicity: "Branco(a)",
+        role: "Administrador",
+        profileImg: "https://randomuser.me/api/portraits/women/92.jpg",
+      },
+      contacts: {},
+      professionalData: {},
+      docs: {},
+      schoolInfo: {},
     },
   };
 });
