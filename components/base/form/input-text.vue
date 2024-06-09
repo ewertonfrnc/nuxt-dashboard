592939045ad4f1488fedb8cb799cfb4dc978fac7
Ewerton Franco <ewerton.silva@usemobile.xyz>
2024-06-09 14:56:13 -0300

    feat: ✨ fixed recover code form

--- components/base/form/input-text.vue
+++ components/base/form/input-text.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>
-    
-  </div>
+  <label class="input">
+    <span class="caption__primary" v-text="label" />
+
+    <VeeField v-slot="{ field, errorMessage }" :name="name">
+      <InputText
+        v-bind="field"
+        v-model="field.value"
+        :class="['input__field body__primary', errorMessage && 'error']"
+        :disabled="disabled"
+        :placeholder="placeholder"
+        :readonly="readonly"
+      />
+    </VeeField>
+
+    <Transition>
+      <ErrorMessage :name="name" class="caption__secondary input__error" />
+    </Transition>
+  </label>
 </template>
 
-<script lang="ts" setup>
-
+<script lang="ts">
+export default {
+  props: {
+    name: { type: String, default: "", required: true },
+    label: { type: String, default: "", required: false },
+    placeholder: { type: String, default: "", required: false },
+    readonly: { type: Boolean, default: false, required: false },
+    disabled: { type: Boolean, default: false, required: false },
+  },
+};
 </script>
-
-<style>
-
-</style>
